5b6653843d6470496aa21abeb5c6b24424be725e
mzhang-code <zhangmengyu.grad@gmail.com>
2020-07-01 11:04:00 -0700

    Fix LSTM unittest

--- fixtures/models.py
+++ fixtures/models.py
@@ -65,13 +65,13 @@ class ScalarNet(nn.Module):
 
 
 class LSTMNet(nn.Module):
-    """ LSTM model. """
+    """ Batch-first LSTM model. """
 
     def __init__(self, vocab_size=20, embed_dim=300, hidden_dim=512, num_layers=2):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.encoder = nn.LSTM(embed_dim, hidden_dim, num_layers=num_layers)
+        self.encoder = nn.LSTM(embed_dim, hidden_dim, num_layers=num_layers, batch_first=True)
         self.decoder = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, x):
