a91d296d63f237eefe032f86fa8f53f729e41deb
Tyler Yep <tyep@cs.stanford.edu>
2020-10-27 17:47:29 -0700

    convert setup.py to metadata

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
 """ setup.py """
-import setuptools
+from setuptools import setup
 
-with open("README.md") as fh:
-    LONG_DESCRIPTION = fh.read()
-
-setuptools.setup(
-    name="torch-summary",
-    version="1.4.3",
-    author="Tyler Yep @tyleryep",
-    author_email="tyep10@gmail.com",
-    description="Model summary in PyTorch, based off of the original torchsummary.",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tyleryep/torch-summary",
-    packages=["torchsummary"],
-    keywords=(
-        "torch pytorch torchsummary torch-summary summary keras deep-learning ml "
-        "torchinfo torch-info visualize model statistics layer"
-    ),
-    python_requires=">=3.6",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
