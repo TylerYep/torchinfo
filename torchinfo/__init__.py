9bb2f2e94f9241026ce29bca5d27dc10715e135c
cjekel <cjekel@gmail.com>
2021-09-28 17:15:24 -0700

    add __version__ to torchinfo

--- torchinfo/__init__.py
+++ torchinfo/__init__.py
@@ -4,3 +4,10 @@ from .model_statistics import ModelStatistics
 from .torchinfo import summary
 
 __all__ = ("ModelStatistics", "summary", "ALL_COLUMN_SETTINGS", "ALL_ROW_SETTINGS")
+
+import pkg_resources as _pkg_resources
+
+try:
+    __version__ = _pkg_resources.get_distribution('torchinfo').version
+except Exception:
+    __version__ = 'unknown'
